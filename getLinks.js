11960c80daf058b8eeccafad9317d5c7df80fb50
reloadercf <elcfm1@gmail.com>
2023-05-24 21:54:37 -0600

    feat: :art: feat(links): add function extract links of file .md

---
+++ getLinks.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+function getLinks(uri) {
+  const data = fs.readFileSync(uri, "utf8");
+  
+  const getMdLinksFormatMd = data.match(/!?\[([^\]]*)\]\(([^\)]+)\)/gm);
+  const onlyLinks = getMdLinksFormatMd.reduce((acc, currentLink) => {
+    if (currentLink.includes('http://') || currentLink.includes('https://')) {
+      const urlRegex = /\[(.*?)\]\((.*?)\)/;
+      const matches = currentLink.match(urlRegex);
+      if (matches) {
+        const [,text, url] = matches;
+        acc.push({text,url});
+      }
+    }
+    return acc;
+  }, []);
+  
+  return onlyLinks;
+}
+
+console.log(getLinks('./README.md'));
