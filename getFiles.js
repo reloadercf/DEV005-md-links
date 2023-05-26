04330da26da27e418d954ed705a3a0600ff58f14
reloadercf <elcfm1@gmail.com>
2023-05-25 21:27:35 -0600

    feat: :building_construction: feat: Add recursion to get files from foders

--- getFiles.js
+++ getFiles.js
@@ -1,29 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
-const paths = [];
-const getFiles = (uri) => {
+const getFiles = (uri, paths = []) => {
   if (fs.existsSync(uri) && fs.statSync(uri).isFile() && path.extname(uri) === '.md') {
     if (path.isAbsolute(uri)) return [uri];
     return [path.resolve(uri)];
   }
   if (fs.existsSync(uri) && fs.lstatSync(uri).isDirectory()) {
-    fs.readdir(uri, (err, files) => {
-      if (err) {
-        return `no allowed read folder: ${err}`;
+    const folder = fs.readdirSync(uri);
+    folder.forEach((file) => {
+      const filePath = path.join(uri, file);
+      const currentFile = fs.statSync(filePath);
+      if (currentFile.isDirectory()) {
+        getFiles(filePath, paths);
+      } else if (path.extname(filePath) === '.md') {
+        paths.push(path.resolve((filePath)));
       }
-      files.forEach((file) => {
-        if (fs.statSync(uri).isFile() && path.extname(uri) === '.md') paths.push(file);
-        if (fs.lstatSync(uri).isDirectory()) console.log(path.resolve(file));
-        if (fs.lstatSync(uri).isDirectory()) getFiles(path.resolve(file));
-      });
     });
     return paths;
   }
   return undefined;
 };
-
-console.log(getFiles('/Users/macbookpro/Developer/personal/DEV005-md-links/ejemplo'));
 
 module.exports = {
   getFiles,
